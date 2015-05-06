6a326874b20976f4e34fca5325019766e1728487
Mariano Zicavo <marianozicavo@gmail.com>
2015-05-06 00:53:10 -0300

    Creo el esquema HHHH

--- TP Actual/data/script_creacion_inicial.sql
+++ TP Actual/data/script_creacion_inicial.sql
@@ -1,19 +1,21 @@
+USE GD1C2015
+
 BEGIN /* *************** CREACION DEL SCHENMA *************** */
-	IF NOT EXISTS (SELECT [name] FROM [sys].[schemas] WHERE [name] = 'nuestrogrupo')
-		EXECUTE ('CREATE SCHEMA nuestrogrupo AUTHORIZATION gd;');		--nombre del grupo?
+	IF NOT EXISTS (SELECT [name] FROM [sys].[schemas] WHERE [name] = 'HHHH')
+		EXECUTE ('CREATE SCHEMA HHHH AUTHORIZATION gd;');		--nombre del grupo?
 END
 GO
 
 BEGIN /* *************** BORRADO DE TABLAS *************** */
-	IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'logins')
-		DROP TABLE logins;
-	IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'usuarios')
-		DROP TABLE usuarios;
+	IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'HHHH' AND TABLE_NAME = 'logins')
+		DROP TABLE HHHH.logins;
+	IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'HHHH' AND TABLE_NAME = 'usuarios')
+		DROP TABLE HHHH.usuarios;
 END
 GO
 
 BEGIN /* *************** CREACION DE TABLAS *************** */
-	CREATE TABLE usuarios(
+	CREATE TABLE HHHH.usuarios(
 		id_usuario INT IDENTITY(1,1) PRIMARY KEY,
 		usuario NVARCHAR(30) UNIQUE,
 		contrasena CHAR(44) NOT NULL,
@@ -21,8 +23,8 @@ BEGIN /* *************** CREACION DE TABLAS *************** */
 		estado NVARCHAR DEFAULT 'H' CHECK (estado IN ('H','I','B')) -- habilitado, inhabilitado, baja
 	)
 
-	CREATE TABLE logins(
-		id_usuario INT FOREIGN KEY REFERENCES usuarios(id_usuario),
+	CREATE TABLE HHHH.logins(
+		id_usuario INT FOREIGN KEY REFERENCES HHHH.usuarios(id_usuario),
 		fecha DATETIME,
 		exito BIT,
 		numeroDeFallo INT,
@@ -32,61 +34,61 @@ END
 GO
 
 BEGIN /* *************** MIGRACION *************** */
-	INSERT INTO usuarios (usuario,contrasena)
+	INSERT INTO HHHH.usuarios (usuario,contrasena)
 		VALUES ('admin','5rhwUL/LgUP8uNsBcKTcntANkE3dPipK0bHo3A/cm+c=');
 END
 GO
 
 BEGIN /* *************** BORRADO DE STORED PROCEDURES *************** */
 	IF EXISTS (SELECT 1 FROM sys.sysobjects WHERE name='loginProc')
-		DROP PROCEDURE loginProc;
+		DROP PROCEDURE HHHH.loginProc;
 END
 GO
 
 /* *************** CREACION DE STORED PROCEDURES *************** */
-CREATE PROCEDURE loginProc
+CREATE PROCEDURE HHHH.loginProc
     @usu nvarchar(30), 
     @contra char(44)
 AS 
 	DECLARE @id_usuario INT
 	
-	SET @id_usuario = (SELECT id_usuario FROM usuarios WHERE usuario = @usu AND estado != 'B')
+	SET @id_usuario = (SELECT id_usuario FROM HHHH.usuarios WHERE usuario = @usu AND estado != 'B')
 	IF @id_usuario IS NULL
 		BEGIN
 			RAISERROR ('No existe ese usuario',16,1)
 			RETURN
 		END
 		
-	IF (SELECT estado FROM usuarios WHERE id_usuario = @id_usuario) = 'I'
+	IF (SELECT estado FROM HHHH.usuarios WHERE id_usuario = @id_usuario) = 'I'
 		BEGIN
 			RAISERROR ('Este usuario esta inhabilitado. Contacte a un administrador del sistema.',16,1)
 			RETURN
 		END
 	
-	IF (SELECT contrasena FROM usuarios WHERE id_usuario = @id_usuario) = @contra
+	IF (SELECT contrasena FROM HHHH.usuarios WHERE id_usuario = @id_usuario) = @contra
 		BEGIN
-			INSERT INTO logins (id_usuario, fecha, exito,numeroDeFallo)
+			INSERT INTO HHHH.logins (id_usuario, fecha, exito,numeroDeFallo)
 				VALUES (@id_usuario,GETDATE(),1,0)
-			UPDATE usuarios
+			UPDATE HHHH.usuarios
 				SET intentosFallidos = 0
 				WHERE id_usuario = @id_usuario
 			SELECT @id_usuario			 --devuelvo el numero de usuario para agregarlo a la sesion
 		END
 	ELSE
 		BEGIN	
-			UPDATE usuarios
+			UPDATE HHHH.usuarios
 				SET intentosFallidos = intentosFallidos + 1
 				WHERE id_usuario = @id_usuario
 				
-			INSERT INTO logins (id_usuario, fecha, exito,numeroDeFallo)
-				VALUES (@id_usuario,GETDATE(),0,(SELECT intentosFallidos FROM usuarios WHERE id_usuario = @id_usuario))
+			INSERT INTO HHHH.logins (id_usuario, fecha, exito,numeroDeFallo)
+				VALUES (@id_usuario,GETDATE(),0,(SELECT intentosFallidos FROM HHHH.usuarios WHERE id_usuario = @id_usuario))
 					
 			RAISERROR ('Contrasena incorrecta.',16,1)
 			
-			IF (SELECT intentosFallidos FROM usuarios WHERE id_usuario = @id_usuario) = 3
+			IF (SELECT intentosFallidos FROM HHHH.usuarios WHERE id_usuario = @id_usuario) = 3
 				BEGIN
 					RAISERROR ('Ha introducido su contrasena mal 3 veces, por lo que su cuenta se ha inhabilitado. Contacte a un administrador del sistema.',16,1)
-					UPDATE usuarios
+					UPDATE HHHH.usuarios
 						SET estado = 'I'
 						WHERE id_usuario = @id_usuario
 				END
